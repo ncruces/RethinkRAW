6548c57419b6709e2e948f5d92c6a24a38b43b02
Nuno Cruces <ncruces@gmail.com>
2019-02-07 19:28:08 +0000

    Lazy load images.

---
+++ assets/gallery.js
@@ -0,0 +1,26 @@
+void function () {
+
+let images = document.querySelectorAll("img.lazy");
+
+if ("IntersectionObserver" in window) {
+    let observer = new IntersectionObserver(entries => {
+        for (let entry of entries) {
+            if (entry.isIntersecting) {
+                let img = entry.target;
+                img.src = img.dataset.src;
+                img.classList.remove("lazy");
+                observer.unobserve(img);
+            }
+        };
+    });
+
+    for (let img of images) {
+        observer.observe(img);
+    };    
+} else {
+    for (let img of images) {
+        img.src = img.dataset.src;
+    };    
+}
+
+}()
