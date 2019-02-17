b3c6c20adc1683ad0408b6d3829ff501dd6a6496
Nuno Cruces <ncruces@gmail.com>
2019-02-17 02:43:24 +0000

    Towards own XMP.

---
+++ xmp/scan.go
@@ -0,0 +1,31 @@
+package xmp
+
+import (
+	"bytes"
+	"io"
+)
+
+const xpacket_begin = "<?xpacket begin="
+const xpacket_end = "<?xpacket end="
+const xpacket_sufix = `"w"?>`
+const xpacket_id = "W5M0MpCehiHzreSzNTczkc9d"
+
+func splitPacket(data []byte, atEOF bool) (advance int, token []byte, err error) {
+	if begin := bytes.Index(data, []byte(xpacket_begin)); begin >= 0 {
+		if end := bytes.Index(data[begin+len(xpacket_begin):], []byte(xpacket_end)); end >= 0 {
+			if last := begin + len(xpacket_begin) + end + len(xpacket_end) + len(xpacket_sufix); last < len(data) {
+				if bytes.Contains(data[begin:begin+50], []byte(xpacket_id)) {
+					return last, data[begin:last], nil
+				}
+			}
+		}
+		advance = begin
+	} else {
+		advance = len(data) - len(xpacket_begin) + 1
+	}
+
+	if atEOF {
+		return 0, nil, io.EOF
+	}
+	return advance, nil, nil
+}
