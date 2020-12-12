d72b61e6e829a4490f7069570b624b85e8251547
Nuno Cruces <ncruces@users.noreply.github.com>
2020-12-12 00:29:58 +0000

    RAW photos.

--- http_dialog.go
+++ http_dialog.go
@@ -19,7 +19,7 @@ func init() {
 		"*.CRW *.NEF *.RAF *.ORF *.MRW *.DCR *.MOS *.RAW *.PEF *.SRF *.DNG *.X3F *.CR2 *.ERF *.SR2"+
 		"*.KDC *.MFW *.MEF *.ARW *.NRW *.RW2 *.RWL *.IIQ *.3FR *.FFF *.SRW *.GPR *.DXO *.ARQ *.CR3",
 		" ")
-	filters = zenity.FileFilter{Name: "RAW files", Patterns: pattern}
+	filters = zenity.FileFilter{Name: "RAW photos", Patterns: pattern}
 	for _, ext := range pattern[1:] {
 		extensions[ext[1:]] = struct{}{}
 	}
